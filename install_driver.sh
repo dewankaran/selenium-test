d07bef70138b17710196fe0f17474b57ceadd120
dk <111999070+dewankaran@users.noreply.github.com>
2022-09-08 13:37:15 -0400

    Add files via upload

--- install_driver.sh
+++ install_driver.sh
@@ -1,7 +1,7 @@
 # Download and Install chromedriver
-wget -N https://chromedriver.storage.googleapis.com/73.0.3683.68/chromedriver_linux64.zip -O /root/chromedriver_linux64.zip
-unzip /root/chromedriver_linux64.zip
-rm /root/chromedriver_linux64.zip
+wget -N https://chromedriver.storage.googleapis.com/73.0.3683.68/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+rm chromedriver_linux64.zip
 sudo mv -f chromedriver /usr/local/bin/chromedriver
 sudo chown root:root /usr/local/bin/chromedriver
 sudo chmod 0755 /usr/local/bin/chromedriver
